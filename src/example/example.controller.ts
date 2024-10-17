23606abcfcb0e534e34bfa05592d7720e03e632c
Lysander <lysander_jp@outlook.com>
2024-10-17 15:35:43 +0900

    git init ˆ

---
+++ src/example/example.controller.ts
@@ -0,0 +1,10 @@
+
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('example')
+export class ExampleController {
+    @Get()
+    getExample(): string {
+        return 'Hello, NestJS!';
+    }
+}
